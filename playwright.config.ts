56d8f9d922e7950b14db57e5a5bf231d9635d84e
Joey Quandt <joey.r.quandt@gmail.com>
2026-01-10 15:06:16 +0100

    fix playwright error

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'bun run build',
+    command: 'bun run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
