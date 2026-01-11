81342eb53e4a9c522fb0835edfdebbf9d87de98c
Joey Quandt <joey.r.quandt@gmail.com>
2026-01-11 20:01:42 +0100

    framer motion fixes, lint fixes

--- src/components/details/Hero.tsx
+++ src/components/details/Hero.tsx
@@ -61,6 +61,7 @@ export default function Hero({ data, type }: DetailProps) {
         <div>
           {/* <WatchProvider id={data.id} type={type} /> */}
           {data.profile_path && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={
                 data.profile_path
